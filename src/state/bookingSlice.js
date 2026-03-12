748eb7b2b9cbae5d035cb3cb5317e177f50529f2
sowndarya1208-pixel <sowndarya1208@gmail.com>
2026-03-12 20:33:59 +0530

    Add bookingSlice for managing passenger state

--- src/state/bookingSlice.js
+++ src/state/bookingSlice.js
@@ -1,1 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
 
+const bookingSlice = createSlice({
+  name: "booking",
+  initialState: {
+    passenger: null
+  },
+  reducers: {
+    setPassenger: (state, action) => {
+      state.passenger = action.payload;
+    }
+  }
+});
+
+export const { setPassenger } = bookingSlice.actions;
+export default bookingSlice.reducer;

69846e46e1c61adc3d9626f4390dfa7966b4504f
sowndarya1208-pixel <sowndarya1208@gmail.com>
2026-03-12 20:55:49 +0530

    Update Confirmation.js

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,1 +1,29 @@
+import React from "react";
+import { useSelector } from "react-redux";
 
+const Confirmation = ({ goHome }) => {
+
+  const passenger = useSelector((state) => state.booking.passenger);
+
+  return (
+    <div>
+
+      <h2>Booking Confirmed</h2>
+
+      {passenger && (
+        <div>
+          <p>Name: {passenger.name}</p>
+          <p>Email: {passenger.email}</p>
+          <p>Phone: {passenger.phone}</p>
+        </div>
+      )}
+
+      <button onClick={goHome}>
+        Go Home
+      </button>
+
+    </div>
+  );
+};
+
+export default Confirmation;

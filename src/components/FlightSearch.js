6c9f1fe4fa9a520c9e1e087d2796dd34a746601e
sowndarya1208-pixel <sowndarya1208@gmail.com>
2026-03-12 21:23:31 +0530

    Update import path for bookingSlice

--- src/components/FlightSearch.js
+++ src/components/FlightSearch.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { setFlight } from "../redux/bookingSlice";
+import { setFlight } from "../state/bookingSlice";
 
 const FlightSearch = ({ goBooking }) => {
 
